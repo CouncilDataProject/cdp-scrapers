d1130952078d23a0d3f9882aacc244ee89cf1fa7
Sung Cho <sung.w.cho@pm.me>
2021-07-13 18:36:00 -0700

    finally able to get video url

--- cdp_scrapers/instances/seattle.py
+++ cdp_scrapers/instances/seattle.py
@@ -54,14 +54,36 @@ def get_event_video(ev_site_url: str) -> str:
     parse web page at ev_site_url and return url for video if found
     ev_site_url is EventInSiteURL
     '''
-    # broad try to simply return None if fail to get video url
+    # broad try to simply return None if any statement below fails
     try:
         # EventInSiteURL (= MeetingDetail.aspx) has a td tag with a certain id pattern containing url to video
         with urlopen(ev_site_url) as resp:
-            soup = BeautifulSoup(resp.read(),'html.parser')
-            # TODO: maybe find 'a' with id something like ct\S+_ContentPlaceHolder\S*_hypVideo and return href?
-            #       <a id="ctl00_ContentPlaceHolder1_hypVideo" .../> seems to be unique within the entire meeting web page
-            return soup.find('td', id = re.compile(r'ct\S+_ContentPlaceHolder1_tdVideoX')).a['href']
+            soup = BeautifulSoup(resp.read(), 'html.parser')
+            # this gets us the url for the web PAGE containing the video
+            video_page_url = soup.find('a', id = re.compile(r'ct\S*_ContentPlaceHolder\S*_hypVideo'), class_ = 'videolink')['href']
+
+        with urlopen(video_page_url) as resp:
+            # now load the page to get the actual video url
+            soup = BeautifulSoup(resp.read(), 'html.parser')
+
+        # <script>
+        # ...
+        # playerInstance.setup({
+        # sources: [
+        #     {
+        #         file: "//video.seattle.gov/media/council/fin_070921_2612127V.mp4",
+        #         label: "Auto"
+        #     }
+        # ],
+        # ...
+
+        # use this pattern to get the script tag containing the video url
+        # and to also get the url itself from the script tag text
+        video_url_regex = re.compile(r'playerInstance\.setup\s*\(\s*\{\s*sources\:\s*\[\s*\{\s*file\:\s*\"([^\"\n]+)')
+        return 'https:' + video_url_regex.search(soup.find('script', text = video_url_regex).string).group(1)
+
+        # TODO: get transcript (subtitles)
+
     except:
         return None
 
@@ -74,11 +96,11 @@ def get_events() -> List[EventIngestionModel]:
         client_name,
         # for the past 2 days
         # will address duplicates later
-        #begin = datetime.datetime.utcnow() - datetime.timedelta(days = 2),
-        #end = datetime.datetime.utcnow(),
+        begin = datetime.datetime.utcnow() - datetime.timedelta(days = 2),
+        end = datetime.datetime.utcnow(),
         # NOTE: use the range below to retrieve events with 1 containing video
-        begin = datetime.datetime(year = 2021, month = 7, day = 9),
-        end = datetime.datetime(year = 2021, month = 7, day = 10),
+        #begin = datetime.datetime(year = 2021, month = 7, day = 9),
+        #end = datetime.datetime(year = 2021, month = 7, day = 10),
     ):
         try:
             session_time = datetime.datetime.strptime(legistar_ev['EventAgendaLastPublishedUTC'], '%Y-%m-%dT%H:%M:%S.%f')
