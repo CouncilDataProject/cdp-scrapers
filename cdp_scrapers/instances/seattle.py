52fe6be611808e55680aa69180340309f6443004
Sung Cho <sung.w.cho@pm.me>
2021-07-14 21:20:01 -0700

    finally able to get full urls for videos and captions. now creating Session per video since we are given a list of videos, even if almost always just 1

--- cdp_scrapers/instances/seattle.py
+++ cdp_scrapers/instances/seattle.py
@@ -49,10 +49,11 @@ def get_event_minutes(legistar_ev_items: List[Dict]) -> List[EventMinutesItem]:
     return minutes
 
 
-def get_event_video(ev_site_url: str) -> str:
+def get_event_video(ev_site_url: str) -> List[Dict]:
     '''
-    parse web page at ev_site_url and return url for video if found
+    parse web page at ev_site_url and return url for video and captions if found
     ev_site_url is EventInSiteURL
+    [{'video_uri' : 'https://video.mp4', 'caption_uri' : 'https://caption.vtt'}, ...]
     '''
     # broad try to simply return None if any statement below fails
     try:
@@ -76,16 +77,45 @@ def get_event_video(ev_site_url: str) -> str:
         #     }
         # ],
         # ...
+        # tracks: [{
+        #     file: "documents/seattlechannel/closedcaption/2021/fin_070921_2612127.vtt",
+        #     label: "English",
+        #     kind: "captions",
+        #     "default": true
+        # }
+        # 
+        #  ], 
+        # ...
 
-        # use this pattern to get the script tag containing the video url
-        # and to also get the url itself from the script tag text
-        video_url_regex = re.compile(r'playerInstance\.setup\s*\(\s*\{\s*sources\:\s*\[\s*\{\s*file\:\s*\"([^\"\n]+)')
-        return 'https:' + video_url_regex.search(soup.find('script', text = video_url_regex).string).group(1)
+        # entire script tag text that has the video player setup call
+        video_script_text = soup.find('script', text = re.compile(r'playerInstance\.setup')).string
+        # playerSetup({...
+        player_arg_start = re.search(r'playerInstance\.setup\((\{)', video_script_text).start(1)
+        # ...}
+        video_json_blob = video_script_text[player_arg_start : player_arg_start + re.search(r'\)\;\s*\n\s*playerInstance', video_script_text[player_arg_start:]).start(0)]
 
-        # TODO: get transcript (subtitles)
+        # not smart enough to make a cool one-time regex to get all the 'file's in 'sources'
+        sources_start = video_json_blob.find('sources:')
+        sources_end = video_json_blob.find('],', sources_start)
+        video_uris = re.findall(r'file\:\s+\"([^\"]+)', video_json_blob[sources_start : sources_end])
+
+        captions_start = video_json_blob.find('tracks:')
+        captions_end = video_json_blob.find('],', captions_start)
+        caption_uris = re.findall(r'file\:\s+\"([^\"]+)', video_json_blob[captions_start : captions_end])
 
     except:
-        return None
+        return []
+
+    list_uri = []
+    for i in range(len(video_uris)):
+        list_uri.append({
+            # NOTE: video url includes the domain video.seattle.gov
+            #       while caption url is a path with respect to seattlechannel.org
+            'video_uri'   : 'https:' + video_uris[i],
+            'caption_uri' : 'http://seattlechannel.org/' + caption_uris[i]
+        })
+
+    return list_uri
 
 
 def get_events() -> List[EventIngestionModel]:
@@ -96,11 +126,11 @@ def get_events() -> List[EventIngestionModel]:
         client_name,
         # for the past 2 days
         # will address duplicates later
-        begin = datetime.datetime.utcnow() - datetime.timedelta(days = 2),
-        end = datetime.datetime.utcnow(),
+        #begin = datetime.datetime.utcnow() - datetime.timedelta(days = 2),
+        #end = datetime.datetime.utcnow(),
         # NOTE: use the range below to retrieve events with 1 containing video
-        #begin = datetime.datetime(year = 2021, month = 7, day = 9),
-        #end = datetime.datetime(year = 2021, month = 7, day = 10),
+        begin = datetime.datetime(year = 2021, month = 7, day = 9),
+        end = datetime.datetime(year = 2021, month = 7, day = 10),
     ):
         try:
             session_time = datetime.datetime.strptime(legistar_ev['EventAgendaLastPublishedUTC'], '%Y-%m-%dT%H:%M:%S.%f')
@@ -109,16 +139,32 @@ def get_events() -> List[EventIngestionModel]:
         except:
             session_time = None
 
+        sessions = []
+
+        # TODO: Session per video_uri/caption_uri ok?
+        for uri in get_event_video(legistar_ev['EventInSiteURL']):
+            sessions.append(
+                Session(
+                    session_datetime = session_time,
+                    session_index = 0,
+                    video_uri = uri['video_uri'],
+                    caption_uri = uri['caption_uri'],
+                )
+            )
+        else:
+            sessions.append(
+                Session(
+                    session_datetime = session_time,
+                    session_index = 0,
+                    video_uri = None,
+                )
+            )
+
         evs.append(
             EventIngestionModel(
                 body = Body(name = legistar_ev['EventBodyName']),
                 event_minutes_items = get_event_minutes(legistar_ev['EventItems']),
-
-                sessions = [Session(
-                    session_datetime = session_time,
-                    session_index = 0,
-                    video_uri = get_event_video(legistar_ev['EventInSiteURL']),
-                )],
+                sessions = sessions,
             )
         )
 
