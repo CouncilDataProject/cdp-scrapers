cd88e0b89fef4c1a2415ceb74abf7aca472cf830
Sung Cho <sung.w.cho@pm.me>
2021-07-20 20:29:22 -0700

    more linting. no 'except' without specifying specific Exception(s)

--- cdp_scrapers/instances/seattle.py
+++ cdp_scrapers/instances/seattle.py
@@ -284,87 +284,95 @@ class SeattleScraper(LegistarScraper):
         super().__init__('seattle')
 
     def get_video_uris(self, legistar_ev: Dict) -> List[Dict]:
-        # broad try to simply return empty list if any statement below fails
         try:
             # EventInSiteURL (= MeetingDetail.aspx) has a td tag
             # with a certain id pattern containing url to video
             with urlopen(legistar_ev[LEGISTAR_EV_SITE_URL]) as resp:
                 soup = BeautifulSoup(resp.read(), 'html.parser')
-                # this gets us the url for the web PAGE containing the video
-                video_page_url = soup.find(
-                    'a',
-                    id=re.compile(r'ct\S*_ContentPlaceHolder\S*_hypVideo'),
-                    class_='videolink'
-                )['href']
+        except URLError or HTTPError:
+            return []
 
+        try:
+            # this gets us the url for the web PAGE containing the video
+            video_page_url = soup.find(
+                'a',
+                id=re.compile(r'ct\S*_ContentPlaceHolder\S*_hypVideo'),
+                class_='videolink'
+            )['href']
+        # catch if find() didn't find video web page url (no <a id=... href=.../>)
+        except KeyError:
+            return []
+
+        try:
             with urlopen(video_page_url) as resp:
                 # now load the page to get the actual video url
                 soup = BeautifulSoup(resp.read(), 'html.parser')
-
-            # <script>
-            # ...
-            # playerInstance.setup({
-            # sources: [
-            #     {
-            #         file: "//...mp4",
-            #         label: "Auto"
-            #     }
-            # ],
-            # ...
-            # tracks: [{
-            #     file: "documents/seattlechannel/closedcaption/2021/...vtt",
-            #     label: "English",
-            #     kind: "captions",
-            #     "default": true
-            # }
-            # 
-            #  ], 
-            # ...
-
-            # entire script tag text that has the video player setup call
-            video_script_text = soup.find(
-                'script',
-                text=re.compile(r'playerInstance\.setup')
-            ).string
-            # playerSetup({...
-            #             ^
-            player_arg_start = re.search(
-                r'playerInstance\.setup\((\{)',
-                video_script_text
-            ).start(1)
-            # ...});
-            #     ^
-            # playerInstance... # more playerInstance code
-            video_json_blob = video_script_text[
-                player_arg_start :
-                player_arg_start + re.search(
-                    r'\)\;\s*\n\s*playerInstance',
-                    video_script_text[player_arg_start:]
-                ).start(0)
-            ]
-
-            # not smart enough to make one-line regex for all the 'file's in 'sources'
-            videos_start = video_json_blob.find('sources:')
-            videos_end = video_json_blob.find('],', videos_start)
-            # as shown above, url will start with // so prepend https:
-            video_uris = [
-                'https:' + i for i in re.findall(
-                    r'file\:\s*\"([^\"]+)',
-                    video_json_blob[videos_start : videos_end],
-                )
-            ]
-
-            captions_start = video_json_blob.find('tracks:')
-            captions_end = video_json_blob.find('],', captions_start)
-            caption_uris = [
-                'https://www.seattlechannel.org/' + i for i in re.findall(
-                    r'file\:\s*\"([^\"]+)',
-                    video_json_blob[captions_start : captions_end],
-                )
-            ]
-
-        except:
+        except URLError or HTTPError:
             return []
+
+        # <script>
+        # ...
+        # playerInstance.setup({
+        # sources: [
+        #     {
+        #         file: "//...mp4",
+        #         label: "Auto"
+        #     }
+        # ],
+        # ...
+        # tracks: [{
+        #     file: "documents/seattlechannel/closedcaption/2021/...vtt",
+        #     label: "English",
+        #     kind: "captions",
+        #     "default": true
+        # }
+        # 
+        #  ], 
+        # ...
+
+        # entire script tag text that has the video player setup call
+        video_script_text = soup.find(
+            'script',
+            text=re.compile(r'playerInstance\.setup')
+        ).string
+
+        # playerSetup({...
+        #             ^
+        player_arg_start = re.search(
+            r'playerInstance\.setup\((\{)',
+            video_script_text
+        ).start(1)
+
+        # ...});
+        #     ^
+        # playerInstance... # more playerInstance code
+        video_json_blob = video_script_text[
+            player_arg_start :
+            player_arg_start + re.search(
+                r'\)\;\s*\n\s*playerInstance',
+                video_script_text[player_arg_start:]
+            ).start(0)
+        ]
+
+        # not smart enough to make one-line regex for all the 'file's in 'sources'
+        videos_start = video_json_blob.find('sources:')
+        videos_end = video_json_blob.find('],', videos_start)
+        # as shown above, url will start with // so prepend https:
+        video_uris = [
+            'https:' + i for i in re.findall(
+                r'file\:\s*\"([^\"]+)',
+                video_json_blob[videos_start : videos_end],
+            )
+        ]
+
+        captions_start = video_json_blob.find('tracks:')
+        captions_end = video_json_blob.find('],', captions_start)
+        caption_uris = [
+            'https://www.seattlechannel.org/' + i for i in re.findall(
+                r'file\:\s*\"([^\"]+)',
+                video_json_blob[captions_start : captions_end],
+            )
+        ]
 
         iter = range(len(video_uris))
         list_uri = []
