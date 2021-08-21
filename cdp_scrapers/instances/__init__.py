53ea1b43cffa012abfb554191641f5fade586d47
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-08-21 15:05:23 -0700

    Construct scraper class dict from imports

--- cdp_scrapers/instances/__init__.py
+++ cdp_scrapers/instances/__init__.py
@@ -3,3 +3,27 @@
 """
 Individual scratchpad and maybe up-to-date CDP instance scrapers.
 """
+
+from pkgutil import iter_modules
+import importlib
+import inspect
+from typing import Dict
+
+from cdp_scrapers.legistar_utils import LegistarScraper
+
+###############################################################################
+
+
+SCRAPERS: Dict[str, LegistarScraper] = {}
+for submodule in iter_modules(__path__):
+    # Import the submodule
+    mod = importlib.import_module(f"{__name__}.{submodule.name}")
+
+    # Get the scraper from the module
+    for a, member_cls in inspect.getmembers(mod, inspect.isclass):
+        if (
+            issubclass(member_cls, LegistarScraper)
+            and member_cls is not LegistarScraper
+        ):
+            # Attach the class to scrapers with it's municipality name
+            SCRAPERS[member_cls.MUNICIPALITY_SLUG] = member_cls
