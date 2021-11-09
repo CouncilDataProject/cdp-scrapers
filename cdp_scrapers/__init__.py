d7eb5ce24dbccdf3b7a8bbf420440e51f0fe60e7
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-11-08 20:41:19 -0800

    Bump version: 0.2.2 → 0.2.3

--- cdp_scrapers/__init__.py
+++ cdp_scrapers/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Jackson Maxfield Brown"
 __email__ = "jmaxfieldbrown@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 def get_module_version():
