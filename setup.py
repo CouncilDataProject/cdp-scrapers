cce88c2663ce02d4a36b3b4a3b43ec7833752ecc
Sung Cho <sung.w.cho@protonmail.com>
2022-01-10 19:45:43 -0800

    duplicate bumpversion call

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-scrapers",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="version="0.3.3"",
+    version="0.3.3",
     zip_safe=False,
 )
