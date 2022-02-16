2cd40732bf806769a81bb69c2994b2fe72a3cb64
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-02-16 08:42:21 -0800

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-scrapers",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.3.4",
+    version="0.3.5",
     zip_safe=False,
 )
