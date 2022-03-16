b2e40503e172ff0eb5a5ebdd23e32a6133685e61
Sung Cho <sung.w.cho@protonmail.com>
2022-03-16 16:37:26 -0700

    Bump version: 0.3.8 → 0.3.9

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-scrapers",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.3.8",
+    version="0.3.9",
     zip_safe=False,
 )
