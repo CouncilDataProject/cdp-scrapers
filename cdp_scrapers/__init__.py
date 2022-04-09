9e356555182d10b7cb33ab92224cf7b894e9b7fd
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-04-09 15:45:47 -0700

    Bump version: 0.3.10 → 0.3.11

--- cdp_scrapers/__init__.py
+++ cdp_scrapers/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Jackson Maxfield Brown"
 __email__ = "jmaxfieldbrown@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.3.10"
+__version__ = "0.3.11"
 
 
 def get_module_version():
--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-scrapers",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.3.10",
+    version="0.3.11",
     zip_safe=False,
 )
