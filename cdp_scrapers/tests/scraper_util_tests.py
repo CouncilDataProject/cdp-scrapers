3e1d550d0765199433a37704d95932e0d07eda03
Shak <shak.ragoler@gmail.com>
2022-06-12 16:02:43 -0700

    removing failed test

--- cdp_scrapers/tests/scraper_util_tests.py
+++ cdp_scrapers/tests/scraper_util_tests.py
@@ -15,8 +15,7 @@ from cdp_scrapers.scraper_utils import str_simplified
         ("test\r\n\ftest", "test\ntest"),
         ("test \t\vtest", "test test"),
         ("M. Lorena Gonz\u00e1lez", "M. Lorena González"),
-        (5, 5),
-        ("Hello", "There")
+        (5, 5)
     ],
 )
 def test_str_simplifed(input_string: str, expected_output: str):
