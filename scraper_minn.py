eb605de17475be3655d779d982b9f89b08da8906
Kunyang <kunyal@uw.edu>
2022-07-27 23:27:03 -0700

    Able to get all urls of valid marked agendas.

--- scraper_minn.py
+++ scraper_minn.py
@@ -1,30 +1,45 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from datetime import datetime
 import re
+import requests as r
+import pandas as pd
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-def get_all_urls(url, startdate='', enddate=''):
-    # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    divided = re.split('\?', url)
-    first_part = divided[0]
-    second_part = divided[1]
-    divided_2 = re.split('&', second_part)
+def get_events(url, start_date, end_date):
+    # Converting dates to url format
+    start_date_as_parameter = convert_date(start_date)
+    end_date_as_parameter = convert_date(end_date)
     
-    sd = 'fd=' + startdate.replace('/', '%2F')
-    ed = 'ed=' + enddate.replace('/', '%2F')
+    # Update the API
+    new_url = re.sub(r'toDate=(.*?)&', 'toDate=' + end_date_as_parameter + '&', \
+                     re.sub(r'fromDate=(.*?)&', 'fromDate=' + start_date_as_parameter + '&', url))
     
-    divided_2[0] = sd
-    divided_2[1] = ed
+    # Request from API and filter all the valid committees
+    committees = pd.DataFrame(r.get(new_url).json())
+    valid_committee_types = ['Independent Committee', 'Standing Committee', 'City Council']
+    valid_agenda_status = ['Committee Report Published', 'Publish', 'Marked Agenda Published']
+    committees = committees[(committees.CommitteeType.isin(valid_committee_types)) & (committees.AgendaStatus.isin(valid_agenda_status))]
     
-    print(divided_2)
-    # driver.get(url)
-    # driver.find_element(by='css selector', value='#resolutionsAdoptedItems tbody tr:nth-child(4) :nth-child(5) a').text
+    # Generate all individual Marked Agenda urls
+    agenda_ids = committees.AgendaId
+    marked_agenda_urls = []
+    for id in agenda_ids:
+        marked_agenda_urls.append('https://lims.minneapolismn.gov/MarkedAgenda/' + str(id))
+    
+    return marked_agenda_urls
+
+
+def convert_date(date):
+    new_date = date.split('/')
+    start_date_as_locale_str = datetime(int(new_date[2]), int(new_date[1]), int(new_date[0])).strftime("%b %d, %Y")
+    return start_date_as_locale_str.replace(" ", "%20")
 
 
 def main():
-    url = "https://lims.minneapolismn.gov/sessionstatistics/resolutionsadopted?fd=01%2F01%2F2022&ed=07%2F22%2F2022&pid=&page=2"
-    get_all_urls(url=url, startdate='01/02/2022', enddate='07/23/2022')
+    d = get_events('https://lims.minneapolismn.gov/Calendar/GetCalenderList?fromDate=May 1, 2021&toDate=Aug 1, 2022&meetingType=0&committeeId=null&pageCount=1000&offsetStart=0&abbreviation=undefined&keywords=', '31/01/2021', '26/07/2022')
+    print(len(d))
+    print(d[:5])
 
 
 if __name__ == "__main__":
