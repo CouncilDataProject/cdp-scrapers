8defc40a4f11b5c398b013abf7980e04313c9987
Joy713 <wenhuz3@uw.edu>
2022-07-29 13:30:39 -0700

    new get event stuff

--- Houston.py
+++ Houston.py
@@ -4,14 +4,13 @@ from cdp_backend.pipeline import ingestion_models
 from datetime import datetime, timedelta
 from typing import Optional, List
 
+#For individual function test
 URL = "https://houston.novusagenda.com/agendapublic//MeetingView.aspx?doctype=Agenda&MinutesMeetingID=0&meetingid=549"
 video_URL = "https://houstontx.new.swagit.com/videos/177384"
 page = requests.get(URL)
 event = BeautifulSoup(page.content, "html.parser")
-
 form1 = event.find('form', id = 'Form1')
-# contain body name & date
-bodyTable = form1.find_all('table')[1].find('table')
+#
 
 
 #Big Functions
@@ -24,7 +23,9 @@ main = BeautifulSoup(main_page.content, "html.parser")
 
 # 1)loop through all date, 2)change each into datetime format, 3)if match, get the 3rd td
 # 4)get the href in first a
-def get_date_main(time: datetime):
+# return the link, make agenda and video url in other function
+# need to change format
+def get_date_mainlink(time: datetime) -> str:
     #all events in a specific year
     main_year = main.find('div', id = 'city-council-2022').find('table', id = 'video-table').find('tbody').find_all('tr')
     time = datetime.strptime(time, '%Y-%m-%d').date() #may need to delete when actually pass datetime
@@ -34,31 +35,30 @@ def get_date_main(time: datetime):
         date = datetime.strptime(date, '%b %d %Y').date()
         if (date == time):
             link = cells[3].find('a')['href']
-            link = 'https://houstontx.new.swagit.com/' + link #may have to return only link and make
-                                                    #agenda and video url in other function
-            #agenda url
-            agenda = link + '/agenda'
-            #video url
-            video = link + '/embed'
-            print(agenda)
-        #print(time)
-    #return date
+            link = 'https://houstontx.new.swagit.com/' + link
+    return link
+
+#agenda url: agenda = link + '/agenda'
+#video url: video = link + '/embed'
+
+def get_agenda(event_time: datetime):
+    agenda_link = get_date_mainlink(event_time) + '/agenda'
+    page = requests.get(agenda_link)
+    event = BeautifulSoup(page.content, "html.parser")
+    form1 = event.find('form', id = 'Form1')
+    return form1
+#what is the type for form1?????????beautifulsoup????????
 
 # parse one event at a specific date
-def get_event(
-    self, 
-    event_time: datetime
-) -> ingestion_models.EventIngestionModel:
-# convert main page string date to datetime: datetime.strptime('Jul 06 2022', '%b %d %Y').date()
-# loop through the whole list in the specific year, find the event that has date match the passed
-# in date, and get the agenda url
-    # the video url is just the href url + "/embed", agenda is url + "/agenda"
-    event = self.get_agenda()
+def get_event(event_time: datetime) -> ingestion_models.EventIngestionModel:
+    event = get_agenda(event_time)
     # things to do after getting the agenda url
     ingestion_models.EventIngestionModel(
-        body = Body(name = self.get_bodyName),
+        #body = ingestion_models.Body(name = get_bodyName(event)),
+        body = ingestion_models.Body('City Council'),
         sessions = 0
     )
+#what is the output for testing purpose???????????????????????????????????
 
 # get all events within time range
 def get_events(
@@ -72,7 +72,9 @@ def get_events(
 
 #Get Functions
 # get body name
-def get_bodyName(self, event: BeautifulSoup):
+def get_bodyName(event: BeautifulSoup):
+    # contain body name & date
+    bodyTable = event.find_all('table')[1].find('table')
     if 'CITY COUNCIL' in bodyTable.text:
         return 'City Council'
     else:
@@ -80,9 +82,4 @@ def get_bodyName(self, event: BeautifulSoup):
 
 
 
-#def print_date(date: datetime):
-    print(date == '2022-03-22')
-# get session video url
-print(get_date_main('2022-07-26'))
-#print(datetime.strptime('2022-03-22', '%Y-%m-%d').date())
-#datetime.strptime('Mar 22 2022', '%b %d %Y').date() == 
+print(get_event('2022-07-26'))
