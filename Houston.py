7d40e2055734348337cf7e5b9c3298b329f3ae07
Joy713 <wenhuz3@uw.edu>
2022-07-29 21:53:39 -0700

    nearly done with get_events

--- Houston.py
+++ Houston.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import requests
 from cdp_backend.pipeline import ingestion_models
 from datetime import datetime, timedelta
@@ -12,67 +12,9 @@ event = BeautifulSoup(page.content, "html.parser")
 form1 = event.find('form', id = 'Form1')
 #
 
-
-#Big Functions
-main_URL = "https://houstontx.new.swagit.com/views/408"
-main_page = requests.get(main_URL)
-main = BeautifulSoup(main_page.content, "html.parser")
-
-# get date from main page
-#only find a specific year. different year by div id = 'city-council-2022', later do get_diff_year
-
-# 1)loop through all date, 2)change each into datetime format, 3)if match, get the 3rd td
-# 4)get the href in first a
-# return the link, make agenda and video url in other function
-# need to change format
-def get_date_mainlink(time: datetime) -> str:
-    #all events in a specific year
-    main_year = main.find('div', id = 'city-council-2022').find('table', id = 'video-table').find('tbody').find_all('tr')
-    time = datetime.strptime(time, '%Y-%m-%d').date() #may need to delete when actually pass datetime
-    for year in main_year:
-        cells = year.find_all('td')
-        date = cells[1].text.replace(',', '').strip()
-        date = datetime.strptime(date, '%b %d %Y').date()
-        if (date == time):
-            link = cells[3].find('a')['href']
-            link = 'https://houstontx.new.swagit.com/' + link
-    return link
-
-#agenda url: agenda = link + '/agenda'
-#video url: video = link + '/embed'
-
-def get_agenda(event_time: datetime):
-    agenda_link = get_date_mainlink(event_time) + '/agenda'
-    page = requests.get(agenda_link)
-    event = BeautifulSoup(page.content, "html.parser")
-    form1 = event.find('form', id = 'Form1')
-    return form1
-#what is the type for form1?????????beautifulsoup????????
-
-# parse one event at a specific date
-def get_event(event_time: datetime) -> ingestion_models.EventIngestionModel:
-    event = get_agenda(event_time)
-    # things to do after getting the agenda url
-    ingestion_models.EventIngestionModel(
-        #body = ingestion_models.Body(name = get_bodyName(event)),
-        body = ingestion_models.Body('City Council'),
-        sessions = 0
-    )
-#what is the output for testing purpose???????????????????????????????????
-
-# get all events within time range
-def get_events(
-    self,
-    begin: Optional[datetime] = None,
-    end: Optional[datetime] = None
-) -> list:
-    for day in range((end - begin).days): 
-        self.get_event(begin + timedelta(days=day))
-
-
-#Get Functions
-# get body name
-def get_bodyName(event: BeautifulSoup):
+#Individual Get Functions
+# get body name, done
+def get_bodyName(event: Tag):
     # contain body name & date
     bodyTable = event.find_all('table')[1].find('table')
     if 'CITY COUNCIL' in bodyTable.text:
@@ -82,4 +24,73 @@ def get_bodyName(event: BeautifulSoup):
 
 
 
-print(get_event('2022-07-26'))
+
+
+#Big Functions
+main_URL = "https://houstontx.new.swagit.com/views/408"
+main_page = requests.get(main_URL)
+main = BeautifulSoup(main_page.content, "html.parser")
+
+# get date from main page
+#only find a specific year. different year by div id = 'city-council-2022', later do get_diff_year
+# 1)loop through all date, 2)change each into datetime format, 3)if match, get the 3rd td
+# 4)get the href in first a
+# return the link, make agenda and video url in other function
+# need to change format
+def get_date_mainlink(time) -> str:
+    #all events in a specific year
+    main_year = main.find('div', id = 'city-council-2022').find('table', id = 'video-table').find('tbody').find_all('tr')
+    #time = datetime.strptime(time, '%Y-%m-%d').date() #may need to delete when actually pass datetime
+    link = 'https://houstontx.new.swagit.com/'
+    for year in main_year:
+        cells = year.find_all('td')
+        date = cells[1].text.replace(',', '').strip()
+        date = datetime.strptime(date, '%b %d %Y').date()
+        if (date == time):
+            link_post = cells[3].find('a')['href']
+            link = link + link_post
+    return link
+
+#agenda url: agenda = link + '/agenda'
+#video url: video = link + '/embed'
+
+# get agenda for a specific date, done
+def get_agenda(event_time: datetime):
+    link = get_date_mainlink(event_time)
+    agenda_link = link + "/agenda"
+    page = requests.get(agenda_link)
+    event = BeautifulSoup(page.content, "html.parser") #type: BeautifulSoup
+    form1 = event.find('form', id = 'Form1') #type: Tag
+    return form1
+
+# parse one event at a specific date, done
+def get_event(event_time: datetime) -> ingestion_models.EventIngestionModel:
+    agenda = get_agenda(event_time)
+    # just basic body and sessions for now, add more after get_events done
+    event = ingestion_models.EventIngestionModel(
+        body = ingestion_models.Body(name = get_bodyName(agenda), is_active=True),
+        sessions = [
+            ingestion_models.Session(
+                session_datetime = event_time,
+                video_uri = get_date_mainlink(event_time) + '/embed',
+                session_index = 0
+        )
+        ]
+    )
+    return event
+
+# get all events within time range
+#now need to deal with cases with no events on that date
+def get_events(begin, end) -> list:
+    events = []
+    begin = datetime.strptime(begin, '%Y-%m-%d').date()
+    end = datetime.strptime(end, '%Y-%m-%d').date()
+    for day in range((end - begin).days + 1): 
+        date = begin + timedelta(days=day)
+        event = get_event(date)
+        events.append(event)
+    return events
+
+
+#print(get_date_mainlink('2022-07-26'))
+print(get_events('2022-05-10', '2022-05-10'))
