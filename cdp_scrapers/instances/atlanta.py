41daac5b7e011d42c148ed0f638f2ab057db4b26
Cici <peiwenfeng0825@gmail.com>
2022-08-23 10:10:41 -0700

    sponsor need fix

--- cdp_scrapers/instances/atlanta.py
+++ cdp_scrapers/instances/atlanta.py
@@ -1,5 +1,3 @@
-from ast import For, Store
-from http.client import CONTINUE
 from typing import Tuple
 from xml.dom.minidom import Element
 import selenium
@@ -17,70 +15,200 @@ from dateutil.parser import parse
 
 MINUTE_INDEX = [chr(i) for i in range(ord('A'),ord('Z')+1)]
 
-def convert_decision_constant(decision:str) -> db_constants:
+def get_single_person(driver:webdriver, member_name:str) -> ingestion_models.EventIngestionModel:
+    seat_role = driver.find_element(By.CLASS_NAME, 'titlewidget-subtitle').text
+    member_role = "Member"
+    member_seat_name = "District"
+    member_seat_area = "Citywide"
+    if ('President' in seat_role):
+        member_role = 'President'
+        member_seat_name = 'President'
+    elif ('Post' in seat_role): #need post number?
+        name_list = seat_role.split(' ')
+        member_seat_name = 'Post ' + name_list[1]
+    else:
+        area_list = seat_role.split(' ')
+        member_seat_area = area_list[1]
+    member_pic = driver.find_element(By.CSS_SELECTOR, '.image_widget img').get_attribute('src')
+    temp_email = driver.find_element(By.XPATH, "// a[contains(text(),'Click Here')]").get_attribute('href').split(":")
+    member_email = temp_email[1]
+    try:
+        member_details = driver.find_element(By.XPATH, "//*[contains(@id, 'widget_340_')]").text
+    except (selenium.common.exceptions.NoSuchElementException):
+        member_details = driver.find_element(By.XPATH, "//*[contains(@id, 'widget_437_')]").text
+    detail_str = member_details.split('\n')
+    phone_list = [s for s in detail_str if "P" in s]
+    member_phone = phone_list[0].split(': ')[1]
+
+    return ingestion_models.Person(
+        name = member_name,
+        is_active = True,
+        email = member_email,
+        phone = member_phone,
+        picture_uri = member_pic,
+        seat = ingestion_models.Seat(
+            name = member_seat_name,
+            electoral_area = member_seat_area,
+            roles = ingestion_models.Role(
+                title = member_role
+            )
+        )
+    )
+
+def get_person() -> dict:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get('https://citycouncil.atlantaga.gov/council-members')
+    members = driver.find_elements(By.XPATH, '//*[@id="leftNav_2_0_12"]/ul/li')
+    person_dict = {}
+    for member in members:
+        link = member.find_element(By.TAG_NAME, 'a').get_attribute("href")
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get(link)
+        member_name = driver.find_element(By.CLASS_NAME, 'titlewidget-title').text
+        member_model = get_single_person(driver, member_name)
+        driver.quit()
+        person_dict[member_name] = member_model
+    driver.quit()
+    return person_dict
+
+
+PERSONS = get_person()
+
+def convert_mdecision_constant(decision:str) -> db_constants:
     d_constant = decision
-    if ('FAVORABLE' in decision) or ('ADOPTED'in decision) or ('REFERRED' in decision) or ('ACCEPTED' in decision):
-        d_constant = db_constants.VoteDecision.APPROVE
+    if ('FAVORABLE' in decision) or ('ADOPTED'in decision) or ('ACCEPTED' in decision) or ('accepted' in decision):
+        d_constant = db_constants.MatterStatusDecision.ADOPTED
+    elif('REFERRED' in decision) or ('RETURNED' in decision) or ('FILED'in decision) or ('Refer'):
+        d_constant = db_constants.MatterStatusDecision.IN_PROGRESS
     return d_constant
 
 def get_voting_result(driver:webdriver, sub_sections:Element, i:int) -> dict:
+    voting_list = []
     for j in range(1, len(sub_sections)+1):
-        Yes_list = []
-        No_list = []
         sub_content =  driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]")
         sub_content_role = sub_content.find_element(By.CLASS_NAME, "Role").text
         if "AYES" in sub_content_role: 
             v_yes = driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]/td[2]").text
-            Yes_list = v_yes.split(",")
+            Yes_list = v_yes.split(", ")
+            for n in Yes_list:
+                if n in PERSONS:
+                    voting_list.append(ingestion_models.Vote(person = PERSONS.get(n), decision = db_constants.VoteDecision.APPROVE))
+                else:
+                    voting_list.append(ingestion_models.Vote(get_new_person(n), decision = db_constants.VoteDecision.APPROVE))
         if "NAYS" in sub_content_role:
             v_no = driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]/td[2]").text
-            No_list = v_no.split(",")
-    voting_result_dict = {"AYES" : Yes_list, "NAYS": No_list}
-    return voting_result_dict
+            No_list = v_no.split(", ")
+            for n in No_list:
+                if n in PERSONS:
+                    voting_list.append(ingestion_models.Vote(person = PERSONS.get(n), decision = db_constants.VoteDecision.REJECT))
+                else:
+                    voting_list.append(ingestion_models.Vote(get_new_person(n), decision = db_constants.VoteDecision.REJECT))
+        if "ABSENT" in sub_content_role:
+            v_absent = driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]/td[2]").text
+            Absent_list = v_absent.split(", ")
+            for n in Absent_list:
+                if n in PERSONS:
+                    voting_list.append(ingestion_models.Vote(person = PERSONS.get(n), decision = db_constants.VoteDecision.ABSENT_NON_VOTING))
+                else:
+                    voting_list.append(ingestion_models.Vote(get_new_person(n), decision = db_constants.VoteDecision.ABSENT_NON_VOTING))
+        if "AWAY" in sub_content_role:
+            v_away = driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]/td[2]").text
+            Away_list = v_away.split(", ")
+            for n in Away_list:
+                if n in PERSONS:
+                    voting_list.append(ingestion_models.Vote(person = PERSONS.get(n), decision = db_constants.VoteDecision.ABSENT_NON_VOTING))
+                else:
+                    voting_list.append(ingestion_models.Vote(get_new_person(n), decision = db_constants.VoteDecision.ABSENT_NON_VOTING))
+        if "ABSTAIN" in sub_content_role:
+            v_abstain = driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]/td[2]").text
+            Abstain_list = v_abstain.split(", ")
+            for n in Abstain_list:
+                if n in PERSONS:
+                    voting_list.append(ingestion_models.Vote(person = PERSONS.get(n), decision = db_constants.VoteDecision.ABSTAIN_NON_VOTING))
+                else:
+                    voting_list.append(ingestion_models.Vote(get_new_person(n), decision = db_constants.VoteDecision.ABSTAIN_NON_VOTING))
+        if "EXCUSED" in sub_content_role:
+            v_excused = driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr[" + str(j) + "]/td[2]").text
+            Excused_list = v_excused.split(", ")
+            for n in Excused_list:
+                if n in PERSONS:
+                    voting_list.append(ingestion_models.Vote(person = PERSONS.get(n), decision = db_constants.VoteDecision.ABSENT_NON_VOTING))
+                else:
+                    voting_list.append(ingestion_models.Vote(get_new_person(n), decision = db_constants.VoteDecision.ABSENT_NON_VOTING))
+    return voting_list
+
+def get_new_person(name:str) -> ingestion_models.EventIngestionModel:
+    return ingestion_models.Person(
+    name = name,
+    is_active = False
+)
 
 def get_matter_decision(driver:webdriver, i:int)-> Tuple[Element,Element]: #unsure about the type for two return items
     result =  driver.find_element(By.XPATH, "//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table")
     decision = result.find_element(By.CLASS_NAME, "Result").text # vote result
     sub_sections = result.find_elements(By.XPATH, "//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i+1) + "]/td/table/tbody/tr")
-    decision_constant = convert_decision_constant(decision)
+    decision_constant = convert_mdecision_constant(decision)
     return sub_sections, decision_constant
 
 def parse_single_matter(driver:webdriver, matter:Element) -> ingestion_models.EventIngestionModel:
     try:
+        voting_list = []
         test = matter.find_element(By.CLASS_NAME, 'ItemVoteResult').text
-        if ("Held in Committee" not in test) and ("held" not in test): # remove the matters that are not mentioned in the meeting
-            item = matter.find_element(By.CLASS_NAME, 'AgendaOutlineLink').text
-            if (len(item)!=0):
-                matter_name = item[0:9] # name of the matter eg. "22-C-5024", "22-R-3404"
-                matter_title = item[12:] # the paragraph the describes the matter eg. "A COMMUNICATION FROM TONYA GRIER, COUNTY CLERK TO THE FULTON COUNTY BOARD OF COMMISSIONERS..."
-                matter_type = " ".join(re.split('BY |FROM',matter_title)[0].split(' ')[1:-1]) # the type of the matter eg. "COMMUNICATION", "SUBSTITUTE ORDINANCE"
-                link = driver.find_element("link text", item)
-                link.click()
-                # get to the specific page for each matter 
-                s_matter = WebDriverWait(driver,10).until(
-                    EC.presence_of_all_elements_located((By.XPATH,
-                    "//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr"))
+        item = matter.find_element(By.CLASS_NAME, 'AgendaOutlineLink').text
+        if (len(item)!=0):
+            matter_name = item[0:9] # name of the matter eg. "22-C-5024", "22-R-3404"
+            matter_title = item[12:] # the paragraph the describes the matter eg. "A COMMUNICATION FROM TONYA GRIER, COUNTY CLERK TO THE FULTON COUNTY BOARD OF COMMISSIONERS..."
+            matter_type = " ".join(re.split('BY |FROM',matter_title)[0].split(' ')[1:-1]) # the type of the matter eg. "COMMUNICATION", "SUBSTITUTE ORDINANCE"
+            link = driver.find_element("link text", item)
+            link.click()
+            # get to the specific page for each matter 
+            s_matter = WebDriverWait(driver,10).until(
+                EC.presence_of_all_elements_located((By.XPATH,
+                "//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr"))
+            )
+            sponsor_raw = driver.find_element(By.XPATH, "//*[@id=\"tblLegiFileInfo\"]/tbody/tr[1]/td[2]").text 
+            sponsor_list = sponsor_raw.split(', ')
+            sponsors = []
+            for s in sponsor_list:
+                if 'District' in s:
+                    current_temp = s.split(' ')[2:]
+                    current = ' '.join(current_temp)
+                    if current in PERSONS:
+                        sponsors.append(PERSONS.get(current))
+                    else:
+                        sponsors.append(get_new_person(current))
+                elif 'Post' in s:
+                    current = s.split('Large ')[1]
+                    if current in PERSONS:
+                        sponsors.append(PERSONS.get(current))
+                    else:
+                        sponsors.append(get_new_person(current))
+                elif 'President' in s:
+                    current = s.split('President ')[1]
+                    if current in PERSONS:
+                        sponsors.append(PERSONS.get(current))
+                    else:
+                        sponsors.append(get_new_person(current))
+            s_rows = (len(s_matter))
+            for i in range(1, s_rows+1, 2):
+                header =  driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i) + "]")
+                date = header.find_element(By.CLASS_NAME, "Date").text
+                s_word = driver.find_element(By.ID, "ContentPlaceHolder1_lblMeetingDate").text
+                if parse(s_word) == parse(date[:-6]): # match the current meeting date
+                    sub_sections, decision = get_matter_decision(driver, i) # get the decision of the matter
+                    if ("[" in test):
+                        voting_list = get_voting_result(driver, sub_sections, i)
+            return ingestion_models.EventMinutesItem(
+                minutes_item = ingestion_models.MinutesItem(matter_name),
+                matter = ingestion_models.Matter(matter_name, 
+                    matter_type = matter_type,
+                    title = matter_title,
+                    result_status = decision,
+                    sponsors = sponsors
+                ),
+                decision = decision,
+                votes = voting_list
                 )
-                sponsor = driver.find_element(By.XPATH, "//*[@id=\"tblLegiFileInfo\"]/tbody/tr[1]/td[2]").text #sponsor for the matter not done yet
-                s_rows = (len(s_matter))
-                for i in range(1, s_rows+1, 2):
-                    header =  driver.find_element(By.XPATH,"//*[@id=\"ContentPlaceHolder1_divHistory\"]/div/table/tbody/tr[" + str(i) + "]")
-                    date = header.find_element(By.CLASS_NAME, "Date").text
-                    s_word = driver.find_element(By.ID, "ContentPlaceHolder1_lblMeetingDate").text
-                    if parse(s_word) == parse(date[:-6]): # match the current meeting date
-                        sub_sections, decision = get_matter_decision(driver, i) # get the decision of the matter
-                        voting_result_dict = get_voting_result(driver, sub_sections, i)
-                            #voting_result_dict = get_voting_result(driver, i, j)
-                            #print(voting_result_dict)
-                return ingestion_models.EventMinutesItem(
-                    minutes_item = ingestion_models.MinutesItem(matter_name),
-                    matter = ingestion_models.Matter(matter_name, 
-                        matter_type = matter_type,
-                        title = matter_title,
-                        result_status = decision
-                    ),
-                    decision = decision
-                    )
     except (selenium.common.exceptions.NoSuchElementException, selenium.common.exceptions.TimeoutException):
         pass
 
@@ -173,3 +301,13 @@ def get_events(from_dt: datetime, to_dt: datetime) -> list:
     events = get_date(driver, web_url, from_dt, to_dt)
     return events
 
+
+
+event = parse_event('https://atlantacityga.iqm2.com/Citizens/SplitView.aspx?Mode=Video&MeetingID=3588&Format=Minutes')
+web_url = "https://atlantacityga.iqm2.com/Citizens/Calendar.aspx?Frame=Yes"
+events = get_events(datetime.fromisoformat('2022-04-18'), datetime.fromisoformat('2022-04-26'))
+with open("april-18th-auto", "w") as open_f:
+    open_f.write(events[2].to_json(indent=4))
+
+
+# person sometime has middle name sometime dosen't
