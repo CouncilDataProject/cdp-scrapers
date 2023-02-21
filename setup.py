3a9edf84353f910bc3d08bc96df0bb0604c0aeb1
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-02-21 14:11:20 -0800

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-scrapers",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.6.0",
+    version="0.6.1",
     zip_safe=False,
 )
